18cf39d9906f142be146944322a5ccfc3a3192ac
Luis Henrique <luishcc2003@gmail.com>
2023-11-08 17:03:19 -0300

    Alterando URL da logo

--- .storybook/alfabitTema.ts
+++ .storybook/alfabitTema.ts
@@ -3,7 +3,7 @@ import { create } from "@storybook/theming/create";
 export default create({
   base: "dark",
   brandTitle: "Alfabit Design System",
-  brandUrl: "https://github.com/NeiltonSeguins/alfabit-design-system",
+  brandUrl: "https://github.com/luissshc29/Alfabit-Design-System",
   brandImage: "./logo-alfabit-branco.png",
   brandTarget: "_self",
 });
